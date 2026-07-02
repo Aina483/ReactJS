9df5129d9b39e691473eef2f7d6b269e43b0f385
Aina483 <ainakhanna0408@gmail.com>
2026-07-02 15:31:07 +0530

    Added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello buddy!</h1>
-    </div>
+      <h1 className=" flex justify-between p-4 bg-pink-500 text-white">
+        <span>Logo</span>
+        <button className='bg-black'>Login</button>
+      </h1>  
+  </div>
   )
 }
 
 export default App
+ 
